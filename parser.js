4a5b8e7edc5c2dee90b10b326052721fb00ae60a
Jeremy Nevill <jeremy@nevill.net>
2013-11-24 14:12:58 +0000

    Date added.

--- parser.js
+++ parser.js
@@ -2,13 +2,26 @@ start = shortee
 
 shortee
     = amper actor:word space
-    action:word space
-    amount:integer units:word
-    { return {actor:actor,action:action,amount:amount,units:units}}
+action:word space
+amount:integer units:word space
+date:datevalue
+{ return {actor:actor,action:action,
+    amount:amount,units:units, date:date}}
 
 word
     = chars:char+
-    {return chars.join("")}
+{return chars.join("")}
+
+datevalue =
+    day:integer datedivider month:char+ datedivider + year:integer
+{return day + "/" + month.join("") + "/" + year}
+
+integer "integer"
+    = digits:[0-9]+
+{ return parseInt(digits.join(""), 10); }
+
+amper
+    = '@'
 
 char
     = [a-zA-Z0-9]
@@ -16,10 +29,6 @@ char
 space
     = ' '
 
-integer "integer"
-    = digits:[0-9]+
-    { return parseInt(digits.join(""), 10); }
-
-amper
-    = '@'
+datedivider
+    = '/'
 
